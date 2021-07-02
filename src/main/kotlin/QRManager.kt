244f0a1dd406835bff69b97d891f1237b9e05c80
Mirco Fabio Gall <mircofabio.gall@students.bfh.ch>
2021-07-02 11:30:21 +0200

    Done + table is now editable + generate the qr code images depending on the given attribute asynchronous with the Dispatcher.IO
    Notes:
    - implement warning for qr inputs and add dropdown for allowed types for the qr code attributes
    - couldn't get to resize the qr codes to a specific size within the imageview jet
    - for some reason the created images of the qr codes are not valid for the imageView of the javafx/tornadofx. Oddly it works when i load the created image into paint, save it without any change and reload it into such an imageView within my tableview
    - Clean Up Maven

--- src/main/kotlin/QRManager.kt
+++ src/main/kotlin/QRManager.kt
@@ -1,18 +1,41 @@
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
 import javafx.beans.property.SimpleListProperty
+import javafx.collections.ListChangeListener
 import javafx.collections.ObservableList
-import java.io.FileOutputStream
-import java.io.FileReader
-import java.io.IOException
-import java.io.Serializable
-
+import javafx.scene.image.ImageView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 import tornadofx.*
-import tornadofx.getValue
-import tornadofx.setValue
+import java.awt.image.BufferedImage
+import java.io.*
+import javax.imageio.ImageIO
+import java.awt.*
+
+const val saveDir = "data/save/"
+const val qrDir = "data/qr"
+
+//For testing purposes
+fun main() {
+    //val manager = QRManager()
+    //manager.restore()
+    if (File("${qrDir}109208388.jpg").exists()) println("true")
 
 
-class QRManager()  {
+    //writeQRCode(manager.entries.first())
+   // println(manager.entries.first().toString())
+    //manager.save()
+    //manager.writeQRCode()
+}
+
+class QRManager() {
     //The given mutableListOf<OR> is just the data which the entries/entriesProperty
     //Shall be built on you could also just use use a variable provided from a constructor
     //when initializing the class
@@ -20,9 +43,30 @@ class QRManager()  {
     var entries: ObservableList<QR> by entriesProperty
     private val mapper = jacksonObjectMapper()
 
+    init {
+        //As soon a new QR elemnt is added
+        entriesProperty.addListener(ListChangeListener { onListChangeHandler(it) })
+        //entriesProperty.addListener(InvalidationListener { it -> print("changed $it")})
+    }
+
+    private fun onListChangeHandler(change: ListChangeListener.Change<out QR>) {
+        runBlocking(Dispatchers.IO + SupervisorJob()) {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    change.addedSubList.forEach { element ->
+                        if (!File("${qrDir}${element.qrId}.jpg").exists()) {
+                            launch { writeQRCode(element) }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
     fun save() {
         try {
-            FileOutputStream("data/output/test.json").use { out ->
+            FileOutputStream("$saveDir test.json").use { out ->
                 mapper.writerWithDefaultPrettyPrinter().writeValue(out, entries)
             }
         } catch (ex: IOException) {
@@ -32,7 +76,7 @@ class QRManager()  {
 
     fun restore() {
         var jsonStr = ""
-        FileReader("data/output/test.json").use { reader -> reader.forEachLine {jsonStr+=it} }
+        FileReader("$saveDir test.json").use { reader -> reader.forEachLine { jsonStr += it } }
         //val jsonString: String = File("data/output/test.json").readText(Charsets.UTF_8)
         entries.addAll(mapper.readValue<List<QR>>(jsonStr).toObservable())
 
@@ -47,37 +91,88 @@ class QRManager()  {
          */
     }
 
-    fun print() {
-
-
-    }
-
-
     fun fill() {
         for (i in 0..10) {
-            entries.add(QR("name$i", "url", listOf<String>("sd")))
+            entries.add(QR("name$i", "url", listOf<String>("someURL")))
         }
     }
-
 }
-
-
 
 
 class QR(
-    val name: String = "",
-    val type: String = "",
-    val attr: List<String> = listOf<String>()
-) : Serializable 
-/*
-fun main(){
-    val t = mutableListOf<String>()
-    t.add("asdf")
-    val manager = QRManager()
-    manager.restore()
-    manager.entries.add(QR("newenew","",listOf("")))
-    print("sdf")
-
+    var name: String = "",
+    var type: String = "",
+    var attr: List<String> = listOf<String>()
+) : Serializable {
+    @JsonIgnore
+    val qrId = attr.hashCode().toString()
+    override fun toString(): String {
+        return ("name: $name | type: $type | attr: ${attr.joinToString(",")}")
+    }
+    @JsonIgnore
+    var img = ImageView("320807582.jpg")
+    // var img = ImageView("qrTemplate-small.jpg")
+   //var img = if(File("/src/main/resources/qr/109208388.jpg").exists()) ImageView("qr/109208388.jpg") else ImageView("qrTemplate-small.jpg")
 }
 
+/*
+// The image is located in default package of the classpath
+Image image1 = new Image("/flower.png");
+
+// The image is located in my.res package of the classpath
+Image image2 = new Image("my/res/flower.png");
+
+// The image is downloaded from the supplied URL through http protocol
+Image image3 = new Image("http://sample.com/res/flower.png");
+
+// The image is located in the current working directory
+Image image4 = new Image("file:flower.png");
  */
+
+
+
+
+
+
+
+
+//Jet only take the first attribute of attr for the qr code
+private fun writeQRCode(element: QR) {
+    println(Thread.currentThread())
+    val writer = QRCodeWriter()
+    val width = 256
+    val height = 256
+    val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB) // create an empty image
+    val white = 255 shl 16 or (255 shl 8) or 255
+    val black = 0
+    try {
+        val bitMatrix = writer.encode(
+            element.attr.joinToString(),
+            BarcodeFormat.QR_CODE,
+            width,
+            height
+        )
+        for (i in 0 until width) {
+            for (j in 0 until height) {
+                image.setRGB(i, j, if (bitMatrix[i, j]) black else white) // set pixel one by one
+            }
+        }
+        try {
+            ImageIO.write(
+                image, "jpg", File(
+                    "$qrDir${element.qrId}.jpg"
+                )
+            ) // save QR image to disk
+        } catch (e: IOException) {
+            println("IOException. Failed to generate QRCode for : ${element.toString()}")
+            println(e.message)
+            e.printStackTrace()
+        }
+    } catch (e: WriterException) {
+        println("WriterException. Failed to generate QRCode for : ${element.toString()}")
+        println(e.message)
+        e.printStackTrace()
+    }
+}
+
+
--- src/main/kotlin/TornadoFxBuilder.kt
+++ src/main/kotlin/TornadoFxBuilder.kt
@@ -1,20 +1,14 @@
-
-import javafx.beans.Observable
+import javafx.beans.binding.Binding
 import javafx.scene.control.Button
 import javafx.scene.control.TableView
 import javafx.scene.control.TextField
-import javafx.scene.layout.*
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.stage.Stage
-import javafx.util.*
 import tornadofx.*
-import javafx.event.*
-import java.awt.Label
+import java.beans.EventHandler
 import java.util.*
-
-
-import tornadofx.getValue
-import tornadofx.setValue
-
+import javax.swing.Spring.height
 
 class QRCodeGenerator2 : App(QRCodeView2::class) {
     init {
@@ -22,8 +16,8 @@ class QRCodeGenerator2 : App(QRCodeView2::class) {
     }
 
     override fun start(stage: Stage) {
-        stage.width=600.0
-        stage.height=500.0
+        stage.width = 600.0
+        stage.height = 500.0
         super.start(stage)
     }
 
@@ -32,62 +26,63 @@ class QRCodeGenerator2 : App(QRCodeView2::class) {
 
 class QRCodeView2 : View("QRCodeViewer") {
 
-    private val manager = QRManager()
+    //private val manager = QRManager()
+    private val controller = MyController()
 
 
     init {
         FX.locale = Locale.GERMAN
-        FX.messages = ResourceBundle.getBundle("QRCodeGenerator",FX.locale)
-        manager.entries.add(QR("empy","empty",listOf("empty")))
-
-
-
-        print("")
+        FX.messages = ResourceBundle.getBundle("QRCodeGenerator", FX.locale)
+        controller.qrList.add(QR("first", "url", listOf("someURL")))
     }
 
-
-
-
+    var txf_name: TextField by singleAssign()
+    var txf_type: TextField by singleAssign()
+    var txf_attr: TextField by singleAssign()
+    var btn_submit: Button by singleAssign()
 
 
     override val root = vbox {
         //Could also be seperated to a variable
         button(FX.messages["btn_load"]) {
-            action { manager.restore()
-
-            print("sdf")
-            }
+            action { controller.restore() }
 
         }
-
-        tableview(manager.entries) {
-            readonlyColumn(FX.messages["col_name"], QR::name){
-
-            }
-            readonlyColumn(FX.messages["col_type"],QR::type){
-
-            }
-            readonlyColumn(FX.messages["col_attr"],QR::attr){
-
-            }
-
-
-
+        hbox {
+            txf_name = textfield { promptText = "name" }
+            txf_type = textfield { promptText = "type" }
+            txf_attr = textfield { promptText = "attribute , attribute , .." }
+            btn_submit = button(FX.messages["btn_submit"]) { onLeftClick { controller.submit(txf_name.text,txf_type.text,txf_attr.text) }}
         }
 
+
+        tableview(controller.qrList) {
+            column(FX.messages["col_name"], QR::name) {
+                isEditable = true
+            }.makeEditable()
+            column(FX.messages["col_attr"], QR::attr) {
+                isEditable = true
+
+            }
+            readonlyColumn(FX.messages["col_qr"], QR::img) {}
+            //readonlyColumn(FX.messages["col_qr"],QR::imgPath,renderImg())
+            //readonlyColumn(FX.messages["col_qr"],imageview("dasf"))
+        }
+        //imageview("qrTemplate.png"){}
+    }
+}
+
+
+private class MyController() : Controller() {
+    private var manager = QRManager()
+    var qrList = manager.entries
+    fun restore() = manager.restore()
+
+    fun submit(_name: String, _type: String, _attr: String) {
 
 
 
     }
-
-
-
-
-    
-
-
-
-
 }
 
 
